d919a6db37a8468c6d2da050b52ee06281b9feb0
Floria Tsui <floria_tsui@brown.edu>
2022-11-04 20:12:18 -0400

    Fixing changes that got overwritten

--- pkg/tcp/tcp_handler.go
+++ pkg/tcp/tcp_handler.go
@@ -255,7 +255,7 @@ func (t *TCPHandler) Shutdown(sdType int, vc *VTCPConn) error {
 
 // Both socket types will need a close, so pass in socket instead
 // then we can call getTye to get the exact object
-func (t *TCPHandler) Close(socket *Socket) error {
+func (t *TCPHandler) Close(socket Socket) error {
 	// TODO:
 	return nil
 }
